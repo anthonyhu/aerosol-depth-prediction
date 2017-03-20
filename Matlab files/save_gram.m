7d63017e8abf0f09844516547c074d91910ac197
Anthony Hu <anth.hu@gmail.com>
2017-03-20 14:52:17 +0100

    Deleted old files

--- Matlab files/save_gram.m
+++
@@ -1,5 +0,0 @@
-gram_file = 'datasetaerosol_full_Gram_costexpected_kratquadr_log2kp10';
-output_name = 'rquadr_1024.csv';
-
-G = load(gram_file);
-dlmwrite(output_name, G.G, 'delimiter', ',', 'precision', 15);
